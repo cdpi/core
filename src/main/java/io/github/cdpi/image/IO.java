511257424c3cbe6beb4fb488739d13a7cd1fba0a
Fabrice Theytaz <2198753+fabricetheytaz@users.noreply.github.com>
2025-08-01 22:52:46 +0200

    Rien

--- src/main/java/io/github/cdpi/image/IO.java
+++ src/main/java/io/github/cdpi/image/IO.java
@@ -11,7 +11,8 @@ import java.nio.file.Path;
 import javax.imageio.ImageIO;
 import javax.imageio.stream.FileImageOutputStream;
 import javax.imageio.stream.ImageOutputStream;
-
+import io.github.cdpi.Argument;
+import io.github.cdpi.exceptions.NullArgumentException;
 import io.github.cdpi.image.exceptions.UnsupportedImageException;
 
 /**
@@ -19,55 +20,66 @@ import io.github.cdpi.image.exceptions.UnsupportedImageException;
  * 
  * Wrapper pour ImageIO car read() peut retourner null et write() peut retourner false...
  * 
- * @version 0.10.0
- * @since 0.10.0
+ * @version 0.11.0
+ * @since 0.11.0
  */
 public class IO
 	{
 	/**
+	 * @throws NullArgumentException
 	 * @throws IOException
+	 * @throws UnsupportedImageException
 	 * 
-	 * @since 0.10.0
+	 * @since 0.11.0
 	 */
 	public static final BufferedImage read(final File file) throws IOException
 		{
+		Argument.notNull(file);
+
 		return imageNotNull(ImageIO.read(file), UnsupportedImageException.forInvalidFile(file.toString()));
 		}
 
 	/**
+	 * @throws NullArgumentException
 	 * @throws IOException
+	 * @throws UnsupportedImageException
 	 * 
-	 * @since 0.10.0
+	 * @since 0.11.0
 	 */
 	public static final BufferedImage read(final InputStream input) throws IOException
 		{
+		Argument.notNull(input);
+
 		return imageNotNull(ImageIO.read(input), new UnsupportedImageException());
 		}
 
 	/**
+	 * @throws NullArgumentException
 	 * @throws IOException
+	 * @throws UnsupportedImageException
 	 * 
-	 * @since 0.10.0
+	 * @since 0.11.0
 	 */
-	public static final BufferedImage read(final ByteBuffer buffer) throws IOException
+	public static final BufferedImage read(final byte[] bytes) throws IOException
 		{
-		try (final var input = new ByteArrayInputStream(buffer.array()))
+		Argument.notNull(bytes);
+
+		try (final var input = new ByteArrayInputStream(bytes))
 			{
 			return read(input);
 			}
 		}
 
 	/**
+	 * @throws NullArgumentException
 	 * @throws IOException
+	 * @throws UnsupportedImageException
 	 * 
-	 * @since 0.10.0
+	 * @since 0.11.0
 	 */
-	public static final BufferedImage read(final byte[] bytes) throws IOException
+	public static final BufferedImage read(final ByteBuffer buffer) throws IOException
 		{
-		try (final var input = new ByteArrayInputStream(bytes))
-			{
-			return read(input);
-			}
+		return read(Argument.notNull(buffer).array());
 		}
 
 	/**
@@ -97,9 +109,9 @@ public class IO
 		}
 
 	/**
-	 * @since 0.10.0
+	 * @since 0.11.0
 	 */
-	protected static final BufferedImage imageNotNull(final BufferedImage image, final UnsupportedImageException exception)
+	private static final BufferedImage imageNotNull(final BufferedImage image, final UnsupportedImageException exception)
 		{
 		if (image == null)
 			{
